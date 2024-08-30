9bba03478c7af6312e3b8d1772f78fa7c8086c89
MyeongSeop Shin <83411963+shinmyeongsub@users.noreply.github.com>
2024-08-31 01:00:07 +0900

    feat: web server & servlet container

---
+++ src/main/java/hello/container/AppInitV1Servlet.java
@@ -0,0 +1,17 @@
+package hello.container;
+
+import hello.servlet.HelloServlet;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
+
+public class AppInitV1Servlet implements AppInit {
+    @Override
+    public void onStartUp(ServletContext servletContext) {
+        System.out.println("AppInitV1Servlet.onStartUp");
+
+        // 순수 서블릿 코드 등록
+        ServletRegistration.Dynamic servlet =
+                servletContext.addServlet("helloServlet", new HelloServlet());
+        servlet.addMapping("/hello-servlet");
+    }
+}
